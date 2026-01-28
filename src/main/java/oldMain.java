52819058cc1ed4b79b321333384a3cdebe35f418
elliotjohnwu <elliotjohnwu@gmail.com>
2026-01-29 07:05:23 +0800

    Add oldMain for add contactNumbers exercise

---
+++ src/main/java/oldMain.java
@@ -0,0 +1,55 @@
+public class oldMain {
+    static int[] amounts = new int[100];
+    static String[] borrowers = new String[100];
+    static boolean[] isPaid = new boolean[100];
+    static double[] interestRates = new double[100];
+    static String[] contactNumbers = new String[100];
+    static int loansCount = 0;
+
+    static Loan[] loans = new Loan[100];
+
+    public static void addLoan(String borrower, int amount, double interestRate, String contactNumber) {
+        borrowers[loansCount] = borrower;
+        amounts[loansCount] = amount;
+        interestRates[loansCount] = interestRate;
+        contactNumbers[loansCount] = contactNumber;
+        loansCount++;
+    }
+
+    public static void setAsPaid(String borrower) {
+        for (int i = 0; i < loansCount; i++) {
+            if (loans[i].getBorrower().equals(borrower)) {
+                loans[i].setAsPaid();
+            }
+        }
+    }
+
+    public static void listLoans() {
+        for (int i = 0; i < loansCount; i++) {
+            if (!isPaid[i]) {
+                System.out.println(borrowers[i] + ": " + amounts[i]);
+            }
+        }
+    }
+
+    public static double calculateProfitEarned() {
+        double profit = 0;
+        for (int i = 0; i < loansCount; i++) {
+            // if the loan was repaid, add the interest to profit
+            if (isPaid[i]) {
+                profit = profit + (amounts[i] * interestRates[i]);
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        addLoan("Alice Betsy", 100, 0.1, "23457");
+        addLoan("Ben Chee", 50, 0.2, "98745");
+        addLoan("Chris Davis", 30, 0.3, "345645");
+        setAsPaid("Ben Chee");
+        listLoans();
+        System.out.println("profit: " + calculateProfitEarned());
+    }
+}
+
